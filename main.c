689f55933a8941667e1e3b6074f35d30e45e8ff1
breadforhire <126725193+breadforhire@users.noreply.github.com>
2023-08-21 00:47:23 +0000

    added the slope

--- main.c
+++ main.c
@@ -5,15 +5,14 @@
 #include <math.h>
 
 
-/* missing - / + and r^2 / a^2 */
+/*Branche + / - */
 
 __m256  _ZGVcN8v_sinf(__m256 x);
 __m256  _ZGVcN8v_cosf(__m256 x);
 
 typedef struct spiral {
-  __m256 viv, vivsq, va, vasq;
-  __m256 x, y, r0, r1;
-  float b[8];
+  __m256 viv, vivx, vivsq, va, vax, vasq;
+  __m256 x, y, r0, r1, b, a;
 
 
  } spiral;
@@ -23,25 +22,25 @@ void a(){
 
     spiral sx;
 
-    sx.viv = _mm256_set_ps(2.0f, 4.0f, 8.0f, 16.0f, 32.0f, 64.0f, 128.0f, 256.0 );
-    sx.viv = _ZGVcN8v_cosf(sx.viv);
-    sx.vivsq = _mm256_sqrt_ps(sx.viv);
+    sx.vivx = _mm256_set_ps(2.0f, 4.0f, 8.0f, 16.0f, 32.0f, 64.0f, 128.0f, 256.0 );
+    sx.viv = _ZGVcN8v_cosf(sx.vivx);
+    sx.vivsq = _mm256_sqrt_ps(sx.vivx);
     sx.x = _mm256_mul_ps(sx.viv , sx.vivsq);
 
 
-    printf("%12.8f %12.8f \n", sx.x[2], sx.x[3]);
+    printf("%12.8f x %12.8f x \n", sx.x[2], sx.x[3]);
 }
 
 void b(){
 
     spiral sx;
 
-    sx.va = _mm256_set_ps(2.0f, 4.0f, 8.0f, 16.0f, 32.0f, 64.0f, 128.0f, 256.0f);
-    sx.va = _ZGVcN8v_sinf(sx.va);
-    sx.vasq = _mm256_sqrt_ps(sx.va);
+    sx.vax = _mm256_set_ps(2.0f, 4.0f, 8.0f, 16.0f, 32.0f, 64.0f, 128.0f, 256.0f);
+    sx.va = _ZGVcN8v_sinf(sx.vax);
+    sx.vasq = _mm256_sqrt_ps(sx.vax);
     sx.y = _mm256_mul_ps(sx.va , sx.vasq);
 
-    printf("%12.8f %12.8f \n", sx.y[2], sx.y[3]);
+    printf("%12.8f y %12.8f y \n", sx.y[2], sx.y[3]);
 }
 
 void c() {
@@ -54,14 +53,19 @@ void c() {
     sx.r0 = _mm256_mul_ps(sx.x, sx.x);
     sx.r1 = _mm256_mul_ps(sx.y, sx.y);
 
-    sx.r0 = _mm256_sqrt_ps(sx.r0);
-    sx.r1 = _mm256_sqrt_ps(sx.r1);
+    sx.a = _mm256_mul_ps(sx.a, sx.a);
+    sx.b = _mm256_add_ps(sx.r0, sx.r1);
 
-    sx.r0 = _mm256_add_ps(sx.r0, sx.r1);
-   _mm256_storeu_ps(sx.b, sx.r0);
+   printf("%12.8f c, %12.8f c \n", sx.b[2], sx.b[3]);
 
-   printf("%12.8f, %12.8f \n", sx.b[2], sx.b[3]);
+   sx.r0 = _mm256_div_ps(sx.b, sx.a);
 
+   sx.r0 = _ZGVcN8v_cosf(sx.r0);
+   sx.r1 = _ZGVcN8v_sinf(sx.r0);
+
+   sx.a = _mm256_div_ps(sx.r0, sx.r1);
+
+   printf("slope: %12.8f \n", sx.a[0]);
 }
 
 void main(){
